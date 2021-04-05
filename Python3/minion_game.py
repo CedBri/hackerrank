e71e0349428663556e5625e02930fa9ae4d42c89
Cedric Brisson <cedric.brisson@protonmail.com>
2021-04-05 00:45:09 -0400

    Update 05/04/21 00:45

---
+++ Python3/minion_game.py
@@ -0,0 +1,23 @@
+def minion_game(string):
+    # your code goes here
+    vowels_list = set(['a','e','i','o','u','A','E','I','O','U'])
+    consonants = 0
+    vowels = 0
+ 
+    n = len(string)
+    for i, l in enumerate(string):
+        if l in vowels_list:
+            vowels += n-i
+        else:
+            consonants += n-i
+ 
+    if vowels == consonants:
+        print("Draw")
+    elif vowels > consonants:
+        print("Kevin {}".format(vowels))
+    else:
+        print("Stuart {}".format(consonants))
+    
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
